595d01feea521cbb80fb036dff0224f5963c26c2
sitopi <61275085+sitopi@users.noreply.github.com>
2021-06-20 10:44:01 +0900

    不要ファイル削除

--- Assets/SystemConstants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class SystemConstants
-{
-    public const int GAME_ID = 2;
-    public const string RANKING_SERVER_BASE_URL = "http://localhost";
-}
